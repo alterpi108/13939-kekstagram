467d9cfe11adf95e81cd76783340ca8cc75ee148
Natalia S <mio.natali@hotmail.com>
2018-06-12 11:07:22 +0300

    Выносит createPhotoComment в отдельную функцию

--- js/pictures.js
+++ js/pictures.js
@@ -26,6 +26,9 @@ var PHOTO_DESCRIPTIONS = [
 var photoTemplate = document.querySelector('#picture').content;
 var photoItem = document.querySelector('.pictures');
 
+var bigPhoto = document.querySelector('.big-picture');
+bigPhoto.classList.remove('hidden');
+
 var social = document.querySelector('.social');
 social.querySelector('.social__comment-count').classList.add('visually-hidden');
 social.querySelector('.social__loadmore').classList.add('visually-hidden');
@@ -81,29 +84,27 @@ var renderPhotosList = function (photos) {
 };
 
 var fillBigPhoto = function (photoData) {
-  var bigPhoto = document.querySelector('.big-picture');
-  bigPhoto.classList.remove('hidden');
-
   bigPhoto.querySelector('.big-picture__img').src = photoData.url;
   bigPhoto.querySelector('.likes-count').textContent = photoData.likes;
-  bigPhoto.querySelector('.comments-count').textContent = photoData.comments.length;
   bigPhoto.querySelector('.social__caption').textContent = photoData.description;
+};
 
+var createPhotoComment = function (data) {
   socialComments.innerHTML = '';
 
-  var createPhotoComment = function () {
-    var photoComment = '';
+  bigPhoto.querySelector('.comments-count').textContent = data.comments.length;
 
-    for (var i = 0; i < photoData.comments.length; i++) {
-      photoComment += '<li class="social__comment social__comment--text">' + '<img class="social__picture" src="img/avatar-' + getRandomNumber(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">' + '<p class="social__text">' + photoData.comments[i] + '</p>' + '</li>';
-    }
+  var photoComment = '';
 
-    return photoComment;
-  };
+  for (var i = 0; i < data.comments.length; i++) {
+    photoComment += '<li class="social__comment social__comment--text"><img class="social__picture" src="img/avatar-' + getRandomNumber(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35"><p class="social__text">' + data.comments[i] + '</p></li>';
+  }
 
-  socialComments.innerHTML = createPhotoComment();
+  return photoComment;
 };
 
 var allPhotos = generatePhotoList(PHOTO_QUANTITY);
+
 renderPhotosList(allPhotos);
 fillBigPhoto(allPhotos[0]);
+socialComments.innerHTML = createPhotoComment(allPhotos[0]);
